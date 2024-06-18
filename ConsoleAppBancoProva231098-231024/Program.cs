2a278e657702f61cb1ae2ba01d0e9fdc5ba6b7d2
Usuario <Usuario@CPD03-M06>
2024-06-18 11:05:40 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppBancoProva231098-231024/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppBancoProva231098_231024
+{
+    internal class Program
+    {
+        public static double saldo = 0;
+        static void Main(string[] args)
+        {
+            string resp = "SIM";
+            while (resp == "SIM") { 
+            Console.WriteLine("Que ação deseja efetuar?\n Efetuar Depósito-1 \n Efetuar Saque-2 \n Efetuar Pagamento-3 \n Consultar Saldo-4");
+            string ac = Console.ReadLine();
+
+            switch (ac)
+            {
+                case "1":
+                    Deposito();
+                    break;
+                case "2":
+                    Saque();
+                    break; 
+                case "3":                        
+                    Pagamento();
+                    break;
+                case "4":
+                    ConsultaSaldo();
+                    break;
+            }
+            Console.WriteLine("Deseja continuar?");
+            resp = Console.ReadLine().ToUpper();
+            Console.Clear();
+            }
+            Console.ReadKey();
+        }
+
+        static void Deposito()
+        {
+
+            Console.Clear();
+            Console.WriteLine("Insira o valor que deseja depositar: ");
+            double deposito = double.Parse(Console.ReadLine());
+            saldo = saldo+deposito;
+            Console.WriteLine("Valor depositado com sucesso.");
+        }
+
+        static void Saque()
+        {
+            Console.Clear();
+            Console.WriteLine("Insira o valor deseja sacar: ");
+            double saque = double.Parse(Console.ReadLine());
+            if(saldo<saque)
+            {
+                Console.WriteLine("Sem limite disponível");
+            }
+            else
+            {
+                saldo = saldo - saque;
+                Console.WriteLine("Saque efetuado com sucesso.");
+            }
+        }
+
+        static void Pagamento()
+        {
+            Console.Clear();
+            Console.WriteLine("Insira o código do boleto: ");
+            string codigo = Console.ReadLine();
+            Console.WriteLine("Insira o valor do boleto: ");
+            double pagamento = double.Parse(Console.ReadLine());
+            if (saldo < pagamento)
+            {
+                Console.WriteLine("Sem limite disponível");
+            }
+            else
+            {
+                saldo = saldo - pagamento;
+                Console.WriteLine("Pagamento efetuado com sucesso.");
+            }
+        }
+        static void ConsultaSaldo()
+        {
+            Console.Clear();
+            Console.WriteLine("O seu saldo é de: " + saldo.ToString("C"));
+        }
+    }
+}
---
+++ ConsoleAppBancoProva231098-231024/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("ConsoleAppBancoProva231098-231024")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleAppBancoProva231098-231024")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("030a459a-591e-495e-aaf5-1c0b601cae0b")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
